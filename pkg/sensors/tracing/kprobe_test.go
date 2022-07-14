e59e5d3e4c6156b2da26447cbb476a3d54ce559b
Anastasios Papagiannis <tasos.papagiannnis@gmail.com>
2022-07-14 14:34:03 +0000

    Make mountPath const
    This never changes
    
    Signed-off-by: Anastasios Papagiannis <tasos.papagiannnis@gmail.com>

--- pkg/sensors/tracing/kprobe_test.go
+++ pkg/sensors/tracing/kprobe_test.go
@@ -35,9 +35,8 @@ import (
 	"golang.org/x/sys/unix"
 )
 
-var mountPath = "/tmp2"
-
 const (
+	mountPath      = "/tmp2"
 	testConfigFile = "/tmp/tetragon.gotest.yaml"
 	kprobeTestDir  = "/sys/fs/bpf/testObserver/"
 )
